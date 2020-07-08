261cf359d26d3bf8f23a7d066c53553d57a216e4
EdenHuang <edenhuang@openrice.com>
2020-07-08 10:18:37 +0800

    20200708

---
+++ General.Admin/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace General.Admin
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
